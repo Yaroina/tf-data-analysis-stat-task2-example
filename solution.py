132773792ae689ed9789ac1c6f796fce3bce07c9
Ekaterina Eremina <31165386+Yaroina@users.noreply.github.com>
2023-04-05 22:46:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,11 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import chi2
+from scipy.stats import norm
 
 
 chat_id = 395384260 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-   df = 1
-    left = np.var(x) / (26 * chi2.ppf(1 - p / 2, df))
-    right = np.var(x) / (26 * chi2.ppf(p / 2, df))
-    return np.sqrt(left), np.sqrt(right)
+    alpha = 1 - p
+    return 2 * (x.mean() + norm.ppf(alpha / 2) * np.var(x) / np.sqrt(len(x))), 2 * (x.mean() + norm.ppf(1 - alpha / 2) * np.var(x) / np.sqrt(len(x)))
