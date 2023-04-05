80598558b9cd0c0a834a0da1426e5f97fe08e291
Ekaterina Eremina <31165386+Yaroina@users.noreply.github.com>
2023-04-05 22:51:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,5 +7,6 @@ from scipy.stats import norm
 chat_id = 395384260 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    return 2 * (x.mean() + norm.ppf(alpha / 2) * np.var(x) / np.sqrt(len(x))), 2 * (x.mean() + norm.ppf(1 - alpha / 2) * np.var(x) / np.sqrt(len(x)))
+    alpha = p
+    b_alpha = np.quantile(x, alpha)
+    return .026, b_alpha
